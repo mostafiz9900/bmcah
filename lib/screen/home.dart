a1b44f3678d2a57ffb6ea99ad99db650652c1c11
Md. Mostafizur Rahman <mostafiz9900@gmail.com>
2022-01-12 12:51:30 +0600

    button dgn

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,16 +1,129 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade400,
       appBar: AppBar(
         title: Text('HomePage'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('HomePage'),
+      body: SingleChildScrollView(
+        child: Column(  
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: _customButton(
+                    context: context,
+                    name: 'Department',
+                    color: Colors.amber.shade400,
+                    iconData: Icons.departure_board,
+                    onTab: () {},
+                  ),
+                ),
+                Expanded(
+                  child: _customButton(
+                    context: context,
+                    name: 'Department',
+                    color: Colors.blueGrey.shade400,
+                    iconData: Icons.account_balance_wallet,
+                    onTab: () {},
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: _customButton(
+                    context: context,
+                    name: 'Department',
+                    color: Colors.amber.shade400,
+                    iconData: Icons.departure_board,
+                    onTab: () {},
+                  ),
+                ),
+                Expanded(
+                  child: _customButton(
+                    context: context,
+                    name: 'Department',
+                    color: Colors.blueGrey.shade400,
+                    iconData: Icons.account_balance_wallet,
+                    onTab: () {},
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: _customButton(
+                    context: context,
+                    name: 'Department',
+                    color: Colors.amber.shade400,
+                    iconData: Icons.departure_board,
+                    onTab: () {},
+                  ),
+                ),
+                Expanded(
+                  child: _customButton(
+                    context: context,
+                    name: 'Department',
+                    color: Colors.blueGrey.shade400,
+                    iconData: Icons.account_balance_wallet,
+                    onTab: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _customButton({
+    BuildContext? context,
+    @required Color? color,
+    @required String? name,
+    @required IconData? iconData,
+    @required VoidCallback? onTab,
+  }) {
+    return GestureDetector(
+      onTap: onTab,
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+        // width: MediaQuery.of(context!).size.width * .44,
+        height: 120.0,
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(15.0)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              size: 50.0,
+            ),
+            Text(
+              '$name',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
